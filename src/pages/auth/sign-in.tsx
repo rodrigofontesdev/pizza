2db23a043901c9e5bd2b51b18eb7263d6410fe55
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-05-21 12:39:28 -0300

    01: react helmet (seo)

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,3 +1,11 @@
+import { Helmet } from 'react-helmet-async'
+
 export function SignIn() {
-  return <h1>SignIn</h1>
+  return (
+    <>
+      <Helmet title="Login" />
+
+      <h1>SignIn</h1>
+    </>
+  )
 }
