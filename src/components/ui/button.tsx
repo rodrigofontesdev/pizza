f28883bec85100a83db8becff4ab6c277f070bcc
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-06-08 21:26:13 -0300

    02: página login

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -51,7 +51,6 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     )
   },
 )
-
 Button.displayName = 'Button'
 
 export { Button, buttonVariants }
--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom'
 export function AppLayout() {
   return (
     <div>
-      <h1>Cabeçalho</h1>
+      <h1>Dashboard</h1>
 
       <div>
         <Outlet />
