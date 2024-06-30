ac12f83f4a5d1b6143bb668a5919697886c0a5fb
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-06-29 22:02:21 -0300

    05: async assertions e encerramento

--- test/dashboard.e2e-spec.ts
+++ test/dashboard.e2e-spec.ts
@@ -3,15 +3,17 @@ import { expect, test } from '@playwright/test'
 test('display day orders amount metric', async ({ page }) => {
   await page.goto('/', { waitUntil: 'networkidle' })
 
-  expect(page.getByText('20', { exact: true })).toBeVisible()
-  expect(page.getByText('-5% em relação a ontem')).toBeVisible()
+  await expect(page.getByText('20', { exact: true })).toBeVisible()
+
+  await expect(page.getByText('-5% em relação a ontem')).toBeVisible()
 })
 
 test('display month orders amount metric', async ({ page }) => {
   await page.goto('/', { waitUntil: 'networkidle' })
 
-  expect(page.getByText('200', { exact: true })).toBeVisible()
-  expect(
+  await expect(page.getByText('200', { exact: true })).toBeVisible()
+
+  await expect(
     page
       .locator('div')
       .filter({ hasText: /^200-5% em relação ao mês passado$/ })
@@ -22,8 +24,9 @@ test('display month orders amount metric', async ({ page }) => {
 test('display month canceled orders amount metric', async ({ page }) => {
   await page.goto('/', { waitUntil: 'networkidle' })
 
-  expect(page.getByText('5', { exact: true })).toBeVisible()
-  expect(
+  await expect(page.getByText('5', { exact: true })).toBeVisible()
+
+  await expect(
     page
       .locator('div')
       .filter({ hasText: /^5-5% em relação ao mês passado$/ })
@@ -34,6 +37,7 @@ test('display month canceled orders amount metric', async ({ page }) => {
 test('display month revenue metric', async ({ page }) => {
   await page.goto('/', { waitUntil: 'networkidle' })
 
-  expect(page.getByText('R$ 200,00')).toBeVisible()
-  expect(page.getByText('+10% em relação ao mês passado')).toBeVisible()
+  await expect(page.getByText('R$ 200,00')).toBeVisible()
+
+  await expect(page.getByText('+10% em relação ao mês passado')).toBeVisible()
 })
--- test/sign-in.e2e-spec.ts
+++ test/sign-in.e2e-spec.ts
@@ -4,24 +4,26 @@ test('sign in successfully', async ({ page }) => {
   await page.goto('/sign-in', { waitUntil: 'networkidle' })
 
   await page.getByLabel('Seu e-mail').fill('johndoe@example.com')
+
   await page.getByRole('button', { name: 'Acessar painel' }).click()
 
   const toast = page.getByText(
     'Enviamos um link de autenticação para seu e-mail.',
   )
 
-  expect(toast).toBeVisible()
+  await expect(toast).toBeVisible()
 })
 
 test('sign in with wrong credentials', async ({ page }) => {
   await page.goto('/sign-in', { waitUntil: 'networkidle' })
 
   await page.getByLabel('Seu e-mail').fill('wrong@example.com')
+
   await page.getByRole('button', { name: 'Acessar painel' }).click()
 
   const toast = page.getByText('Credenciais inválidas.')
 
-  expect(toast).toBeVisible()
+  await expect(toast).toBeVisible()
 })
 
 test('navigate to new restaurant page', async ({ page }) => {
